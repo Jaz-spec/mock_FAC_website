90f2316c97b9670e52f677d691e0ec53315366a5
Jaz-spec <jaz.edna@proton.me>
2025-10-14 15:11:47 +0100

    chore: adds tailwind and daisyUI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
+   import "../app.css";
    import Footer from "$lib/components/footer.svelte";
    import NavBar from "$lib/components/navBar.svelte";
    import SideBar from "$lib/components/sideBar.svelte";
 
-	let { children } = $props();
+   let { children } = $props();
 </script>
 
 <NavBar/>
-<main>
+<main class="flex justify-around ">
     <SideBar/>
     {@render children?.()}
 </main>
