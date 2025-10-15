21585e5595f003b48253e00e4c09ffe0795d7f2e
Jaz-spec <jaz.edna@proton.me>
2025-10-15 11:12:26 +0100

    feat: basic footer styling

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,12 @@
-<footer class="footer shink dev">
-    <h1>Footer</h1>
+<footer class="shink bg-black p-8 pb-10">
+    <div class="border-t-[10px] border-double border-yellow-300 mb-8"></div>
+    <h1 class="text-white text-center text-base/8">
+        Space4, 2nd Floor 113-115 Fonthill Road, Finsbury Park, London N4 3HH
+        <br/>
+        hello@foundersandcoders.com
+        <br/>
+        Code of Conduct Privacy Policy Apprenticeship Policies Carbon Reduction Plan
+        <br/>
+        © 2025 Founders and Coders. All rights reserved.
+    </h1>
 </footer>
